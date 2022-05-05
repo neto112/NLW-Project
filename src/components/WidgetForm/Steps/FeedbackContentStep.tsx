85116b607d0f01954f8723ccdbd6cfbf14973252
neto112 <cristianorneto@gmail.com>
2022-05-05 13:26:50 -0300

    mai 5

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
 
@@ -30,7 +30,24 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
       </header>
 
       <form className="my-4 w-full">
-        <textarea className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand 500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none" placeholder="Conte com detalhes o que está acontecendo..."></textarea>
+        <textarea
+          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-1 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+          placeholder="Conte com detalhes o que está acontecendo..."
+        />
+
+        <footer className="flex gap-2 mt-2">
+          <button
+            type="button"
+            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+            >
+            <Camera className="w-6 h-6"/>
+          </button>
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors">
+            Enviar feedback
+          </button>
+        </footer>
       </form>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
+const { borderRadius } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       colors: {
         brand: {
+          300: '#996DFF',
           500: '#8257e6',
         }
+      },
+      borderRadius: {
+        md: '4px',
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
